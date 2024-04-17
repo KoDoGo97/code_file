538a51679aa246201c9e6b6eb6c8943f882fa00f
KoDoGo97 <kdg0907123@naver.com>
2024-04-17 16:26:14 +0900

    110

--- model_code/seq2seq.py
+++ model_code/seq2seq.py
@@ -37,3 +37,61 @@ class Lang:
             self.n_words += 1
         else:
             self.word2count[word] += 1
+
+# 유니 코드 문자열을 일반 ASCII로 변환하십시오.
+# https://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+# 소문자, 다듬기, 그리고 문자가 아닌 문자 제거
+
+
+def normalizeString(s):
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    return s
+
+def readLangs(lang1, lang2, reverse=False):
+    print("Reading lines...")
+
+    # 파일을 읽고 줄로 분리
+    lines = open('%s-%s.txt' % (lang1, lang2), encoding='utf-8').read().strip().split('\n')
+
+    # 모든 줄을 쌍으로 분리하고 정규화
+    pairs = [[normalizeString(s) for s in l.split('\t')][0:2] for l in lines]
+    # print(f"============>\n{pairs[0]}")
+    # raise
+
+    # 쌍을 뒤집고, Lang 인스턴스 생성
+    if reverse:
+        pairs = [list(reversed(p)) for p in pairs]
+        input_lang = Lang(lang2)
+        output_lang = Lang(lang1)
+    else:
+        input_lang = Lang(lang1)
+        output_lang = Lang(lang2)
+
+    return input_lang, output_lang, pairs
+
+MAX_LENGTH = 10
+
+eng_prefixes = (
+    "i am ", "i m ",
+    "he is", "he s ",
+    "she is", "she s ",
+    "you are", "you re ",
+    "we are", "we re ",
+    "they are", "they re "
+)
+
+def filterPair(p):
+    return len(p[0].split(' ')) < MAX_LENGTH and \
+        len(p[1].split(' ')) < MAX_LENGTH and \
+        p[1].startswith(eng_prefixes)
+
+def filterPairs(pairs):
+    return [pair for pair in pairs if filterPair(pair)]
