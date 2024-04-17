2724531a294e84bd6afb91fcb5444126a07f6ad9
KoDoGo97 <kdg0907123@naver.com>
2024-04-17 16:13:23 +0900

    011

---
+++ model_code/seq2seq.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals, print_function, division
+from io import open
+import unicodedata
+import string
+import re
+import random
+
+import torch
+import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+#from YN_SS_AIclass.ML_BASE.ML_models import Seq2Seq_ex02
